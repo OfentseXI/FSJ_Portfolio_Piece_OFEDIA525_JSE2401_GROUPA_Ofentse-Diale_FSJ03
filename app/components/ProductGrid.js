f5b1f24ddccbdd3c70e7e28afd96492713ef759c
OfentseXI <dofentse69@gmail.com>
2024-09-11 14:37:46 +0200

    Created new product grid component with a basic layout for the productcard

---
+++ app/components/ProductGrid.js
@@ -0,0 +1,13 @@
+import ProductCard from './productCard';
+
+function ProductGrid({ products }) {
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      {products.map(product => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+}
+
+export default ProductGrid;
