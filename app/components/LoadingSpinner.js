20666226f1092ce8fe236302114d05b725e73b4b
OfentseXI <dofentse69@gmail.com>
2024-09-13 13:03:04 +0200

    Added JSdoc to the loading page

--- app/components/LoadingSpinner.js
+++ app/components/LoadingSpinner.js
@@ -1,9 +1,9 @@
 /**
- * LoadingSpinner component to display a spinner while content is loading.
+ * Loading state to display a spinner while content is loading.
  *
- * @returns {JSX.Element} The rendered loading spinner component.
+ * @returns {JSX.Element} The rendered loading state.
  */
-export default function LoadingSpinner() {
+export default function Loading() {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="loader border-t-4 border-blue-500 rounded-full w-16 h-16 animate-spin"></div>
