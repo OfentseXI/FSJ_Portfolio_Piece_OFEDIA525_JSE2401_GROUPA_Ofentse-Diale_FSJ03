f2bf5fe61dc4bd0d1dcaa93aa58c9538afa0f935
OfentseXI <dofentse69@gmail.com>
2024-09-13 14:49:57 +0200

    Added JSDocs to the loading.js file

--- app/loading.js
+++ app/loading.js
@@ -1,7 +1,15 @@
+/**
+ * Loading component to display a spinner while data is being fetched or the page is loading.
+ * 
+ * This component renders a centered loading spinner that animates using Tailwind CSS utilities.
+ * It is designed to fill the screen and center the spinner both vertically and horizontally.
+ *
+ * @returns {JSX.Element} The rendered loading spinner component.
+ */
 export default function Loading() {
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <div className="loader border-t-4 border-blue-500 rounded-full w-16 h-16 animate-spin"></div>
-      </div>
-    );
-  }
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="loader border-t-4 border-blue-500 rounded-full w-16 h-16 animate-spin"></div>
+    </div>
+  );
+}
