fba636473af4a669f4d0eef49b08d987c503df8b
OfentseXI <dofentse69@gmail.com>
2024-10-08 11:59:23 +0200

    Added enableIndexedDbPersistence calls to firebaseConfig.js

--- app/api/products/firebaseConfig.js
+++ app/api/products/firebaseConfig.js
@@ -10,10 +10,30 @@ const firebaseConfig = {
   appId: process.env.FIREBASE_APP_ID,
 };
 
+enableIndexedDbPersistence(db).catch((err) => {
+  if (err.code == 'failed-precondition') {
+    // Multiple tabs open, persistence can only be enabled in one tab at a time.
+    console.error("Persistence failed: Multiple tabs open");
+  } else if (err.code == 'unimplemented') {
+    // The current browser does not support all of the features required to enable persistence
+    console.error("Persistence is not available in this browser");
+  }
+});
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 // Initialize Firestore
 const db = getFirestore(app);
+
+enableIndexedDbPersistence(db).catch((err) => {
+    if (err.code == 'failed-precondition') {
+      // Multiple tabs open, persistence can only be enabled in one tab at a time.
+      console.error("Persistence failed: Multiple tabs open");
+    } else if (err.code == 'unimplemented') {
+      // The current browser does not support all of the features required to enable persistence
+      console.error("Persistence is not available in this browser");
+    }
+  });
 
 export { db };
