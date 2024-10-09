989cfeb2c3805e90239eead437c2530613778f39
OfentseXI <dofentse69@gmail.com>
2024-10-09 23:50:39 +0200

    Refactor categories route to use NextResponse and added error logging

--- app/api/categories/route.js
+++ app/api/categories/route.js
@@ -5,19 +5,19 @@ import { db } from "../firebaseConfig";
 export async function GET() {
   try {
     const categoriesRef = collection(db, 'categories');
-
     const snapshot = await getDocs(categoriesRef);
 
-    const categories = snapshot.docs.map((doc) => ({
-    //   id: doc.id,
+    const categories = snapshot.docs.map(doc => ({
+      id: doc.id,
       ...doc.data(),
     }));
 
-    return new Response(JSON.stringify(categories), {status: 200})
+    return NextResponse.json({ categories });
   } catch (error) {
+    console.error('Failed to fetch categories:', error);
     return NextResponse.json(
-      {error: "Failed to fetch categories", details: error.message},
-      {status: 500}
-    )
+      { error: "Failed to fetch categories", details: error.message },
+      { status: 500 }
+    );
   }
 }
